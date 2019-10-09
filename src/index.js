b27c1618b18b730d075d7cfcb79cb214bcbc8664
Dziazhurny <grafton999@gmail.com>
2019-10-09 10:03:38 +0300

    feat: solve task

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param preferences - target student focus
+ * @param knowsProgramming - if student can do programming and know basics
+ * @param config - private student ability to perform for different focus modes
+ * @returns number of weeks needed for finish education
+ */
+module.exports = function getTimeForEducation(
+    focus = 'family', 
+    knowsProgramming = true,
+    config = {family: 4}
+    ) {
+    let hoursWithoutBaseJs = 1300;
+     if(knowsProgramming){
+      hoursWithoutBaseJs=800;
+    };
+
+    return Math.ceil(hoursWithoutBaseJs / config[focus]);
+  };
+  
